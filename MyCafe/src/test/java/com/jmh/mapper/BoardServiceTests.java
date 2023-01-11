d1403981fc39e7d14be2514fc2de540d4c67a9eb
HyowolJung <gch00116@gmail.com>
2023-01-11 18:37:23 +0900

    1. CRUD Testing (Presentation Layer)

--- MyCafe/src/test/java/com/jmh/mapper/BoardServiceTests.java
+++ MyCafe/src/test/java/com/jmh/mapper/BoardServiceTests.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.jmh.domain.BoardVO;
 import com.jmh.service.BoardService;
 
 import lombok.Setter;
@@ -27,5 +28,17 @@ public class BoardServiceTests {
 	public void testExist() {
 		log.info(service);
 		assertNull(service);
+	}
+	
+	@Test
+	public void testRegister() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로새로 작성하는 글!");
+		board.setContent("새로새로 작성하는 내용!");
+		board.setWriter("newbie");
+		
+		service.register(board);
+		
+		log.info("생성된 게시물의 번호: " + board.getBno());
 	}
 }
