c61ec4679facd66e8eab3830cdcd8ebff694c5d4
HyowolJung <gch00116@gmail.com>
2023-01-14 18:00:56 +0900

    1. CRUD Tested (Presentation Layer)

--- MyCafe/src/main/java/com/jmh/controller/BoardController.java
+++ MyCafe/src/main/java/com/jmh/controller/BoardController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.jmh.domain.BoardVO;
 import com.jmh.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -14,15 +17,24 @@ import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-@RequestMapping("/board")
+@RequestMapping("/board/*")
 @AllArgsConstructor
 public class BoardController {
 	
 	private BoardService service;
 	
-	@GetMapping("/list")
-	public void list(Model model) {
-		log.info("list");
-		model.addAttribute("list", service.getList());
+//	@GetMapping("/list")
+//	public void list(Model model) {
+//		log.info("list");
+//		model.addAttribute("list", service.getList());
+//	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register: " + board);
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		return "redirect:/board/list";
 	}
 }
