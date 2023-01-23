ca2f5bae197242a060b56d14272a63545765c45a
HyowolJung <gch00116@gmail.com>
2023-01-23 18:39:09 +0900

    1. Processing Board Paging(Spring)

---
+++ MyCafe/src/main/java/com/jmh/domain/Criteria.java
@@ -0,0 +1,16 @@
+package com.jmh.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+	private int pageNum;
+	private int amount;
+	
+	
+	
+}
--- MyCafe/src/main/java/com/jmh/mapper/BoardMapper.java
+++ MyCafe/src/main/java/com/jmh/mapper/BoardMapper.java
@@ -6,10 +6,13 @@ import org.apache.ibatis.annotations.Select;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 import com.jmh.domain.BoardVO;
+import com.jmh.domain.Criteria;
 
 public interface BoardMapper {
 	//@Select("select * from tb1_board where bno > 0")
 	public List<BoardVO> getList();
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
 	
 	public void insert(BoardVO board);
 	public void insertSelectKey(BoardVO board);
@@ -19,4 +22,6 @@ public interface BoardMapper {
 	public int delete(Long bno);
 	
 	public int update(BoardVO board);
+	
+	
 }
