71cf66c802cafc90d27e4f7f481f7ad9226460fc
HyowolJung <gch00116@gmail.com>
2023-02-04 20:43:58 +0900

    1. Processing Mulit-Board System

--- MyCafe/src/main/java/com/jmh/mapper/BoardMapper.java
+++ MyCafe/src/main/java/com/jmh/mapper/BoardMapper.java
@@ -7,17 +7,21 @@ import com.jmh.domain.Criteria;
 
 public interface BoardMapper {
 	//@Select("select * from tb1_board where bno > 0")
+	//게시글 등록
 	public List<BoardVO> getList();
 	public List<BoardVO> getListWithPaging(Criteria cri);
 	
-	
+	//게시글 등록
 	public void insert(BoardVO board);
 	public void insertSelectKey(BoardVO board);
 	
+	//게시글 내용
 	public BoardVO read(Long bno);
 	
+	//게시글 삭제
 	public int delete(Long bno);
 	
+	//게시글 수정
 	public int update(BoardVO board);
 	
 	
--- MyCafe/src/main/java/com/jmh/service/BoardServiceImpl.java
+++ MyCafe/src/main/java/com/jmh/service/BoardServiceImpl.java
@@ -20,25 +20,28 @@ public class BoardServiceImpl implements BoardService{
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
 
+	//게시글 등록
 	@Override
 	public void register(BoardVO board) {
 		log.info("register....." + board);
 		mapper.insertSelectKey(board);
 		
 	}
-
+	//게시글 목록
 	@Override
 	public BoardVO get(Long bno) {
 		log.info("get......" + bno);
 		return mapper.read(bno);
 	}
-
+	
+	//게시글 수정
 	@Override
 	public boolean modify(BoardVO board) {
 		log.info("modify......" + board);
 		return mapper.update(board) == 1;
 	}
-
+	
+	//게시글 삭제
 	@Override
 	public boolean remove(Long bno) {
 		log.info("remove....." + bno);
@@ -50,7 +53,8 @@ public class BoardServiceImpl implements BoardService{
 //		log.info("getList...............");
 //		return mapper.getList();
 //	}
-
+	
+	//게시글 페이징
 	@Override
 	public List<BoardVO> getList(Criteria cri) {
 		log.info("get List with criteria: " + cri);
