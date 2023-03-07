5d3b21fc9f7094f9150f9d882a082179af90febb
HyowolJung <gch00116@gmail.com>
2023-03-07 19:17:14 +0900

    1. Processing Mulit-Board System

--- MyCafe/src/main/java/com/jmh/controller/BoardController.java
+++ MyCafe/src/main/java/com/jmh/controller/BoardController.java
@@ -37,8 +37,8 @@ public class BoardController {
 		model.addAttribute("list", service.getList()); // cri
 		
 		//bgno의 값에 따라서 게시판이 출력해야할 데이터를 구분해야함.
-		if bgno == "1"{
-			model.addAttribute("list", service.getList());
+		if bgno == 1{
+			model.addAttribute("list", service.getList()); 
 		}else {
 			log.info("bgno 체크");
 		}
